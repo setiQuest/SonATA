508a784c014375cd8c84532de23b22d5a7e9ca62
Ken Smolek <kes@smolek.com>
2011-03-31 10:52:41 -0700

    Changed version.

--- sig-pkg/dx/include/System.h
+++ sig-pkg/dx/include/System.h
@@ -48,7 +48,7 @@ using namespace sonata_lib;
 
 namespace dx {
 
-const char *const CODE_VERSION = "kes-1.0.7";
+const char *const CODE_VERSION = "kes-1.0.8";
 
 // defines controlling system configuration
 #define ASSIGN_CPUS			(true)
