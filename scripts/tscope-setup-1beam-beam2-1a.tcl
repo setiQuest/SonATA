dd755af1bae1b8811436f5f8ee59befec07ac414
Jane Jordan <jjordan@seti.org>
2012-01-05 19:55:02 +0000

    And another.

---
+++ scripts/tscope-setup-1beam-beam2-1a.tcl
@@ -0,0 +1,31 @@
+# 1 beam -- beam 1
+
+# tscope-setup-1beam-beam2-1a.tcl
+
+sh echo "SonATA taking array" | mailx -s 'SonATA taking bf2 and ant1a for Testing' -r jjordan@seti.org ata-staff@seti.org
+
+sched set beginfreq 1410.0
+sched set endfreq 1450.2
+sched set beam1 off
+sched set beam2 on
+sched set beam3 off
+sched set tasks prepants,bfreset,bfautoatten,bfinit,caldelay
+
+
+db set name sonatadb
+db set usedb on
+
+tscope set antlistsource param
+tscope set antsprimary  1a
+tscope set antsxpol 1a
+tscope set antsypol 1a
+# both tunings must be the same
+tscope set tuningc 1420.0
+tscope set tuningd 1420.0
+# tscope assign beamxc1 1a
+# tscope assign beamyc1 1a
+ tscope assign beamxd1 1a
+ tscope assign beamyd1 1a
+# tscope assign beamxd2 1a
+# tscope assign beamyd2 1a
+
