d8514aea385357033df137f21562424fb64bd77b
Jane Jordan <jjordan@seti.org>
2013-03-27 19:44:33 +0000

    A couple more unchecked in files.

---
+++ scripts/sonata-cron-begin-da14-session.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# sonata-cron-begin-vger-session.sh
+# save the master-antenna-list.tcl
+cp /home/sonata/sonata_install/scripts/antenna-list-master.tcl /home/sonata/sonata_install/scripts/save-antenna-list-master.tcl
+# use antlist for 8400 mhz
+cp /home/sonata/sonata_install/scripts/antenna-list-master-8400mhz.tcl /home/sonata/sonata_install/scripts/antenna-list-master.tcl
+#run the test to see if we should be running. If not - exit the script.
+/home/sonata/scripts/should_start_obs.rb
+if [ $? -ne 0 ]; then
+# restore antenna-list-master.tcl
+cp /home/sonata/sonata_install/scripts/save-antenna-list-master.tcl /home/sonata/sonata_install/scripts/antenna-list-master.tcl
+  exit
+fi
+#Start the backend process that watches the weather
+/home/sonata/scripts/weather_watch.rb &
+# Kill all the dxs and channelizers in case they weren't shutdown properly
+/home/sonata/sonata_install/scripts/cleanup-chan-dx.tcsh
+
+# start a SonATA observing session
+# start up SSE
+${HOME}/sonata_install/bin/sonata-startup-cron-wrapper.sh -batch
+
+# allow enough time for all components to be ready
+sleep 120
+
+#send command to SSE to start Observing
+${HOME}/sonata_install/bin/sonata-seeker-command-cron-wrapper.sh source ${HOME}/sonata_install/scripts/sonata-start-da14-2beam-8400-8850-obs.tcl
+# restore antenna-list-master.tcl
+cp /home/sonata/sonata_install/scripts/save-antenna-list-master.tcl /home/sonata/sonata_install/scripts/antenna-list-master.tcl
